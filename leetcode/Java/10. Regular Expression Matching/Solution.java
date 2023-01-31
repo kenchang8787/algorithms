836e67186b0d4cac286758897e57307c8b622af7
Ken Chang <63435519+kenchang8787@users.noreply.github.com>
2023-01-31 15:52:37 +0800

    10 解到一半

--- leetcode/Java/10. Regular Expression Matching/Solution.java
+++ leetcode/Java/10. Regular Expression Matching/Solution.java
@@ -1,21 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public boolean isMatch(String s, String p) {
 
         if (s.equals(p))
             return true;
 
-        // 先把所有的 * 的符號改成 .
+        Map<Character, Integer> maps = new HashMap<Character, Integer>();
+
+        // 先把有 * 的字元和位置給記錄起來
         for (int i = 1; i < p.length(); i++) {
 
             if (p.charAt(i) != '*')
                 continue;
 
-            s = s.
+            maps[Character.valueOf(p.charAt(i))] = i;
+
+            char c = p.charAt(i);
+
+            maps[c] = 1;
         }
 
-        for (int i = 0; i < s.length(); i++) {
+        for (int sIndex = 0, pIndex = 0; sIndex < s.length(); sIndex++) {
 
-            if (i >= p.length())
+            if (sIndex >= p.length())
                 return false;
 
             if (p.charAt(pIndex) == '.') {
