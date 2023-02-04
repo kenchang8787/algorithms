b277882c715f82298d499d9572d3801c524971ab
Ken Chang <63435519+kenchang8787@users.noreply.github.com>
2023-02-04 20:52:42 +0800

    Leetcode 17

--- Leetcodes/Java/17. Letter Combinations of a Phone Number/Solution.java
+++ Leetcodes/Java/17. Letter Combinations of a Phone Number/Solution.java
@@ -4,37 +4,38 @@ public class Solution {
 
     final HashMap<Character, String> map = new HashMap<>() {
         {
-            put('2',"abc");put('3',"def");put('4',"ghi");put('5',"jkl");put('6',"mno");put('7',"pqrs");put('8',"tuv");put('9',"wxyz");}};
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }
+    };
 
     public List<String> letterCombinations(String digits) {
 
         List<String> result = new ArrayList<>();
 
-        for (int i = 0; i < digits.length(); i++) {
-            //
+        if (digits.length() != 0) {
+            getChar(result, digits, "");
         }
 
         return result;
-
     }
 
-    private void getCharByIndex(int index,)
+    private void getChar(List<String> result, String digits, String str) {
 
-    private List<String> backtrack(String digits, StringBuilder sb) {
+        if (digits.length() == 0) {
+            result.add(str);
+        } else {
+            String m = map.get(digits.charAt(0));
 
-        if (digits.length() == 0)
-            return new ArrayList<>() {
-                {
-                    add(sb.toString());
-                }
-            };
-
-        String s = map.get(digits.charAt(0));
-
-        for (int i = 0; i < s.length(); i++) {
-            sb.append(s.charAt(i));
-
-            return backtrack(digits, sb);
+            for (int i = 0; i < m.length(); i++) {
+                getChar(result, digits.substring(1), str + m.charAt(i));
+            }
         }
     }
 }
